32ef403a85922e595151334691a5966e4c66da2f
SRIKAR-B-S-S <srikar1705@gmail.com>
2026-09-15 10:06:34 +0530

    feat: add localStorage & minimize option for notepad
    Added localStorage feature which saves the text typed in the notepad, along with a minimize/close feature for the notepad window.

--- script.js
+++ script.js
@@ -15,3 +15,47 @@ searchForm.addEventListener('submit', (e) => {
 
     window.location.href = query;
 });
+
+// JS for Notepad
+const notepadWindow = document.getElementById('notepad-window');
+const notepadIcon = document.getElementById('notepad-icon');
+const notepadEditor = document.getElementById('xp-notepad');
+
+const btnMinimize = document.querySelector('.xp-minimize');
+const btnClose = document.querySelector('.xp-close');
+
+if (btnClose) btnClose.removeAttribute('disabled');
+if (btnMinimize) btnMinimize.removeAttribute('disabled');
+
+// Added Local Storage, so that it can store whatever the user types in Notepad window for later
+// First when the user opens the website, this part of the code loads the previously saved notes:
+const savedNotes = localStorage.getItem('prism_notepad_content');
+if (savedNotes !== null && notepadEditor) {
+    notepadEditor.value = savedNotes;
+}
+
+// This part saves the content as the user types:
+if (notepadEditor) {
+    notepadEditor.addEventListener('input', () => {
+    localStorage.setItem('prism_notepad_content', notepadEditor.value)
+    });
+}
+
+
+//  Minimize/Close Notepad Window features here
+
+function hideNotepad() { //This func. closes the notepad window and shows the icon
+    notepadWindow.classList.add('hidden');
+    notepadIcon.classList.remove('hidden');
+}
+
+function showNotepad() { // This func. shows the notepad window
+    notepadWindow.classList.remove('hidden');
+    if (notepadEditor) notepadEditor.focus();
+}
+
+if (btnMinimize) btnMinimize.addEventListener('click', hideNotepad);
+if (btnClose) btnClose.addEventListener('click', hideNotepad);
+
+if (notepadIcon) notepadIcon.addEventListener('click', showNotepad);
+
